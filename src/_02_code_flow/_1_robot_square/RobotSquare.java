5c3e385cfc8c7b6102498a6d1380497adb1c6834
root <root@204322c23630>
2020-07-14 23:56:30 +0000

    Today I worked on java

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -17,22 +17,25 @@ public class RobotSquare {
 
         // Put the robot's pen down so it can draw
 		//        Use the     rob.penDown();   command.
-
+    	rob.penDown();
         // SPEED. Make the robot move quickly.
 		//        Use the     rob.setSpeed();   command.
-
-
+    	
+    	rob.setSpeed(100);
         // DRAW.  Draw a line
     	//		Use the     rob.move(); command
-
+    	
 
 		// TURN.  Turn 90 degrees
     	//      Use the     rob.turn();  command
-
+    	
 
         // LOOP.  Now put the DRAW and TURN code into a for loop to repeat 4 times.
 		//        Did your Robot draw a square?
-
+    	for(int i=0;i<4;i++) {
+    		rob.move(150);
+    		rob.turn(90);
+    	}
 
     }
 }
--- src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
+++ src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
@@ -9,15 +9,15 @@ public class MadlibsOfTheAmazon {
 		String prompt =  "If you find yourself having to cross a piranha-infested river, here's how to do it...";
 		
 		// Get the player to enter an adjective
-
+		String adjective = "Enter an adjective";
 		// Get the player to enter a type of liquid
-
+		String liquid = "Enter a type of liquid";
 		// Get the player to enter a body part
-
+		String body part = "Enter a body part";
 		// Get the player to enter a verb
-
+		String verb = "Enter a verb";
 		// Get the player to enter a place
-
+		String place = "Enter a place";
 		// The story below has has been written as a group of Strings joined together by + signs.
 		// The story contains place holders, indicated by [** **] which you need to replace with
 		// the values entered by the player.
